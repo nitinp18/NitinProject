fa7413e33e9fd04fd22f1aaf97b6214c3a4efefb
software testing <software testing@DESKTOP-3KAMR5M.mshome.net>
2022-07-25 07:18:43 -0700

    3rd commit

--- src/com/java/github/project/Sample2.java
+++ src/com/java/github/project/Sample2.java
@@ -1,5 +1,12 @@
 package com.java.github.project;
 
-public class Sample2 {
-
+public class Sample2
+{
+public static void main(String[] args)
+{
+	int p=50;
+	int q=30;
+	int sub=p-q;
+	System.out.println(sub);
+}
 }
